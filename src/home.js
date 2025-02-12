17d2800536357638460246b992cc1c9e0d5a66e4
Kexin Huang <khuan24@uwo.ca>
2025-02-11 19:18:11 -0500

    Create HTML mockups of Home and Menu page with stylings.

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 import gusPortrait from "./resources/img/Gus_Happy.png"
    
 export function renderHome() {
-    const contentDiv = document.querySelector("#content-container")
+    // const contentDiv = document.querySelector("#content-container")
     console.log("Rendering Home")
 
     // const image = document.createElement("img");
