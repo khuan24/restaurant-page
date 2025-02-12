f63ac69d509815dad402177343c03e7bba44f11e
Kexin Huang <khuan24@uwo.ca>
2025-02-11 21:20:17 -0500

    Render the menu page via DOM manipulation

--- src/menu.js
+++ src/menu.js
@@ -17,4 +17,93 @@ export function renderMenu() {
     heading.classList.add("menu")
     heading.textContent = "Our Menu"
     contentDiv.appendChild(heading)
+
+    const menuItems = [
+        {
+            name: "Coffee",
+            img: coffeeIcon,
+            price: 300
+        },
+        {
+            name: "Beer",
+            img: beerIcon,
+            price: 400
+        },
+        {
+            name: "Bread",
+            img: breadIcon,
+            price: 120
+        },
+        {
+            name: "Salad",
+            img: saladIcon,
+            price: 220
+        },
+        {
+            name: "Spaghetti",
+            img: spagIcon,
+            price: 240
+        },
+        {
+            name: "Pizza",
+            img: pizzaIcon,
+            price: 600
+        }
+    ]
+
+    let menuCount = 0
+
+    const createMenuItem = (obj) => {
+        const row = document.createElement("tr")
+        console.log("calling menu creation")
+
+        const imgCell = document.createElement("td")
+        const img = document.createElement("img")
+        img.src = obj.img
+        img.alt = obj.name
+        imgCell.appendChild(img)
+        row.appendChild(imgCell)
+
+        const nameCell = document.createElement("td")
+        nameCell.textContent = obj.name
+        row.appendChild(nameCell)
+
+        const priceCell = document.createElement("td")
+        priceCell.classList.add("price")
+        priceCell.textContent = obj.price + "g"
+        row.appendChild(priceCell)
+
+        return row
+    }
+
+    const table = document.createElement("table")
+
+    const rowDrinks = document.createElement("tr")
+    rowDrinks.classList.add("category")
+    const thDrinks = document.createElement("th")
+    thDrinks.colSpan = 3
+    thDrinks.textContent = "Drinks"
+    rowDrinks.appendChild(thDrinks)
+    table.appendChild(rowDrinks)
+
+    while (menuCount<2) {
+        table.appendChild(createMenuItem(menuItems[menuCount]))
+        menuCount++
+    }
+
+    const rowClassics = document.createElement("tr")
+    rowClassics.classList.add("category")
+    const thClassics = document.createElement("th")
+    thClassics.colSpan = 3
+    thClassics.colspan = 3
+    thClassics.textContent = "Classics"
+    rowClassics.appendChild(thClassics)
+    table.appendChild(rowClassics)
+    
+    while (menuCount<menuItems.length) {
+        table.appendChild(createMenuItem(menuItems[menuCount]))
+        menuCount++
+    }
+    
+    contentDiv.appendChild(table)
 }
